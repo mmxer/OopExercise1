6f48efc11d51a95a6c82da30708eb92d8dbf193e
mmxer <mmxer@mail.ru>
2016-11-01 19:11:16 +0300

    Unit tests

---
+++ OopExercise1Tests/HashTableArrayTests.cs
@@ -0,0 +1,46 @@
+﻿using OopExercise1;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OopExercise1.Tests
+{
+    [TestClass()]
+    public class HashTableArrayTests
+    {
+        [TestMethod()]
+        public void FindTest()
+        {
+            HashTableArray repository = new HashTableArray(1, new HashFuctionExample());
+            repository.Add("1", "item 1");
+            Assert.IsNotNull(repository.Find("1"));
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            HashTableArray repository = new HashTableArray(1, new HashFuctionExample());
+            repository.Add("1", "item 1");
+            Assert.IsNotNull(repository.Find("1"));
+            Assert.AreEqual("item 1", repository.Find("1"));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            HashTableArray repository = new HashTableArray(1, new HashFuctionExample());
+            repository.Add("1", "item 1");
+            repository.Remove("1");
+            Assert.IsNull(repository.Find("1"));
+        }
+
+        [TestMethod()]
+        public void HashTableFunctionTest()
+        {
+            HashTableArray repository = new HashTableArray(1, new HashFuctionExample());
+            repository.Add("1", "item 1");
+            repository.HashFunction = new HashFuctionExample();
+            Assert.IsNotNull(repository.Find("1"));
+            Assert.AreEqual("item 1", repository.Find("1"));
+        }
+    }
+}
